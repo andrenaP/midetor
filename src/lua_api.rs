3f4eb60aafc29ec7d2496ef985a5d8b8159a3303
andrenaP <a0993100106@gmail.com>
2026-04-23 13:51:17 +0100

    test

--- src/lua_api.rs
+++ src/lua_api.rs
@@ -5,12 +5,33 @@ use std::rc::Rc;
 
 #[derive(Clone, Debug)]
 pub enum EditorCommand {
+    // Basic Movement
     MoveToTop,
     MoveToBottom,
+    MoveUp,
+    MoveDown,
+    MoveLeft,
+    MoveRight,
+    MoveWordForward,
+    MoveWordBack,
+    MoveHead,
+    MoveEnd,
+
+    // Actions
+    Undo,
+    Redo,
+    Save,
+    NavigateBack,
+    NavigateForward,
+    EnterMode(String),
+    DeleteNextChar,
+    InsertLineBelow,
+    Paste,
+
+    // Existing...
     Echo(String),
     Quit,
     StartSearch(String),
-    // --- NEW COMMANDS ---
     YankLine,
     DeleteLine,
     EnterFileTree,
@@ -19,6 +40,11 @@ pub enum EditorCommand {
     ToggleReadMode,
     OpenWikilink(String),
     ProcessTemplate(String),
+
+    FollowLink,
+    Cancel,
+
+    ChangeStatus(String),
 }
 
 pub struct LuaEditorAPI {
@@ -111,7 +137,16 @@ impl UserData for LuaEditorAPI {
             Ok(())
         });
 
-        // Method to register keybindings from Lua
+        methods.add_method("echo", |_, this, msg: String| {
+            this.command_queue
+                .borrow_mut()
+                .push(EditorCommand::Echo(msg));
+            Ok(())
+        });
+        methods.add_method("quit", |_, this, ()| {
+            this.command_queue.borrow_mut().push(EditorCommand::Quit);
+            Ok(())
+        });
         methods.add_method(
             "map",
             |lua, this, (mode, seq, func): (String, String, Function)| {
@@ -122,5 +157,91 @@ impl UserData for LuaEditorAPI {
                 Ok(())
             },
         );
+
+        methods.add_method("follow_link", |_, this, ()| {
+            this.command_queue
+                .borrow_mut()
+                .push(EditorCommand::FollowLink);
+            Ok(())
+        });
+
+        methods.add_method("cancel", |_, this, ()| {
+            this.command_queue.borrow_mut().push(EditorCommand::Cancel);
+            Ok(())
+        });
+
+        methods.add_method("move", |_, this, direction: String| {
+            let cmd = match direction.as_str() {
+                "up" => EditorCommand::MoveUp,
+                "down" => EditorCommand::MoveDown,
+                "left" => EditorCommand::MoveLeft,
+                "right" => EditorCommand::MoveRight,
+                "word_forward" => EditorCommand::MoveWordForward,
+                "word_back" => EditorCommand::MoveWordBack,
+                "head" => EditorCommand::MoveHead,
+                "end" => EditorCommand::MoveEnd,
+                "top" => EditorCommand::MoveToTop,
+                "bottom" => EditorCommand::MoveToBottom,
+                _ => return Ok(()),
+            };
+            this.command_queue.borrow_mut().push(cmd);
+            Ok(())
+        });
+
+        methods.add_method("undo", |_, this, ()| {
+            this.command_queue.borrow_mut().push(EditorCommand::Undo);
+            Ok(())
+        });
+
+        methods.add_method("redo", |_, this, ()| {
+            this.command_queue.borrow_mut().push(EditorCommand::Redo);
+            Ok(())
+        });
+
+        methods.add_method("save", |_, this, ()| {
+            this.command_queue.borrow_mut().push(EditorCommand::Save);
+            Ok(())
+        });
+
+        methods.add_method("set_mode", |_, this, mode: String| {
+            this.command_queue
+                .borrow_mut()
+                .push(EditorCommand::EnterMode(mode));
+            Ok(())
+        });
+
+        methods.add_method("paste", |_, this, ()| {
+            this.command_queue.borrow_mut().push(EditorCommand::Paste);
+            Ok(())
+        });
+
+        methods.add_method("insert_line_below", |_, this, ()| {
+            this.command_queue
+                .borrow_mut()
+                .push(EditorCommand::InsertLineBelow);
+            Ok(())
+        });
+
+        methods.add_method("navigate_back", |_, this, ()| {
+            this.command_queue
+                .borrow_mut()
+                .push(EditorCommand::NavigateBack);
+            Ok(())
+        });
+
+        methods.add_method("navigate_forward", |_, this, ()| {
+            this.command_queue
+                .borrow_mut()
+                .push(EditorCommand::NavigateForward);
+            Ok(())
+        });
+
+        // This just changes the text at the bottom, without changing the view!
+        methods.add_method("set_status", |_, this, msg: String| {
+            this.command_queue
+                .borrow_mut()
+                .push(EditorCommand::ChangeStatus(msg));
+            Ok(())
+        });
     }
 }
